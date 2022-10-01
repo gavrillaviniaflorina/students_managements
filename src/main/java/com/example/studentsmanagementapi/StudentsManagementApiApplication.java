7b118ec2743b7c2a84fda32cfc40170523bcff4b
lavinia gavril <lavinia.florina.gavril@gmail.com>
2022-10-01 17:54:04 +0300

    updated actions

--- src/main/java/com/example/studentsmanagementapi/StudentsManagementApiApplication.java
+++ src/main/java/com/example/studentsmanagementapi/StudentsManagementApiApplication.java
@@ -22,57 +22,9 @@ public class StudentsManagementApiApplication {
         SpringApplication.run(StudentsManagementApiApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner commandLineRunner(
-//            UserRepository userRepository, BookRepository bookRepository, CourseRepository courseRepository
-//    ){
-//        return args->{
-
-        //   Faker faker=new Faker();
-//
-//
-//            for(int i=0;i<10;i++){
-//                String firstName=faker.name().firstName();
-//                String lastName=faker.name().lastName();
-//                String email=String.format("%s.%s@mycode.edu",firstName,lastName);
-//                User user=new User(faker.name().lastName(),email,faker.book().author());
-//                userRepository.save(user);
-//            }
-
-//
-         // User user=userRepository.findById(7L).get();
-//
-//            for(int i=0;i<10;i++){
-//
-//                LocalDate date=LocalDate.of(faker.number().numberBetween(2010,2022),faker.number().numberBetween(1,12),faker.number().numberBetween(1,30));
-//                Book book=new Book(faker.book().title(),date,faker.educator().secondarySchool());
-//                bookRepository.save(book);
-//
-//            }
-//
-//            userRepository.save(user);
-
-
-//            for(int i=0;i<10;i++){
-//
-//;
-//                Course course=new Course(faker.book().title(),faker.book().author(), faker.educator().university());
-//
-//                user.addCourse(course);
-//            }
-//
-//            userRepository.save(user);
-//
-//
-//
-//            for(int i=0;i<10;i++){
-//                Course course=new Course(faker.book().title(),faker.book().author(), faker.educator().university());
-//
-//                courseRepository.save(course);
-//            }
-//
-//
-//       };
-//    }
-
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository userRepository, BookRepository bookRepository, CourseRepository courseRepository) {
+        return args -> {
+        };
+    }
 }
